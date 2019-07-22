3fa4e904375cd307cb48049212b8164974cfc03b
Jeremy Oglesby <iaminaband@gmail.com>
2019-07-22 10:37:31 -0700

    Image Blur 1 - first pass

---
+++ image_blur_1.rb
@@ -0,0 +1,43 @@
+class Image
+  def initialize rows
+    @rows = rows
+    @height = rows.count
+    row_lengths = 0
+    first_row_length = 0
+    
+    rows.each_with_index do |row, index|
+      if index == 0
+        first_row_length = row.count
+      else
+        row_length = row.count
+        unless first_row_length == row_length
+          puts "All rows are not the same length!"
+        end
+      end
+
+      row_lengths += index == 0 ? first_row_length : row_length
+    end
+
+    @length = row_lengths / rows.count
+  end
+
+  def output_image
+    output = ""
+    @rows.each do |row|
+      output += row.join + "\n"
+    end
+    puts output
+  end
+end
+
+
+# EXECUTE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
