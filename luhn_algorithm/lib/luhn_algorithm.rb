a691daeeb25b06c1081b9e159b08a4c1f03b76b3
Jeremy Oglesby <iaminaband@gmail.com>
2019-09-05 21:40:54 -0700

    Luhn algorithm challenge

---
+++ luhn_algorithm/lib/luhn_algorithm.rb
@@ -0,0 +1,16 @@
+module Luhn
+  def self.valid?(number)
+    digits = number.to_s.split(//).map(&:to_i).reverse
+    sum = 0
+
+    digits.each_with_index do |digit, index|
+      if (index % 2).zero?
+        sum += digit
+      else
+        sum += (digit * 2) >= 10 ? (digit * 2) - 9 : digit * 2
+      end
+    end
+
+    (sum % 10).zero?
+  end
+end
